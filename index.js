4ec03e15bd85202df783fbea47eb8693b872a7f0
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2026-06-12 01:00:02 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { connectDB } = require('./src/config/db');
+const { createBot } = require('./src/bot');
+
+async function main() {
+  await connectDB();
+  const bot = createBot();
+  await bot.launch();
+  console.log('🤖 Bot ishga tushdi!');
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
+
+main().catch(console.error);
