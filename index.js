4c0f61936e3dda774341a94849b596ddb4fc4028
Sarvar Shukirlayev <Shukirlayevs@gmail.com>
2026-06-17 06:17:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const { createBot } = require('./src/bot');
 async function main() {
   await connectDB();
   const bot = await createBot();
-  await bot.launch();
+
+  await bot.launch({
+    allowedUpdates: [
+      'message',
+      'edited_message',
+      'callback_query',
+      'inline_query',
+      'chosen_inline_result',
+      'chat_join_request'
+    ]
+  });
+
   console.log('🤖 Bot ishga tushdi!');
   process.once('SIGINT', () => bot.stop('SIGINT'));
   process.once('SIGTERM', () => bot.stop('SIGTERM'));
